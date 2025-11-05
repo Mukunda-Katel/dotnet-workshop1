940ecf3c77ecf30863c03504ccc140894c4b4b7f
Mukunda-Katel <mukundakatel@gmail.com>
2025-11-05 12:10:37 +0545

    Task 2

---
+++ task2/Program.cs
@@ -0,0 +1,30 @@
+﻿
+class Circle
+{
+    public const double PI = 3.14;
+
+
+    public double CalculateArea(double radius)
+    {
+        return PI * radius * radius;
+    }
+
+
+    public double CalculatePerimeter(double radius)
+    {
+        return 2 * PI * radius;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+        Circle c = new Circle();
+        double radius = 5;
+
+        Console.WriteLine("Area of Circle: " + c.CalculateArea(radius));
+        Console.WriteLine("Perimeter of Circle: " + c.CalculatePerimeter(radius));
+    }
+}
