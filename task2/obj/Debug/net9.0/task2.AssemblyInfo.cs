33900804872af26093ca2f5327ea310deba7aece
Mukunda-Katel <mukundakatel@gmail.com>
2025-11-05 12:18:05 +0545

    Task 4

--- task2/obj/Debug/net9.0/task2.AssemblyInfo.cs
+++ task2/obj/Debug/net9.0/task2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+940ecf3c77ecf30863c03504ccc140894c4b4b7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42aba3eaffc6c6bd9bf0b81e8fb5be9e010eb164")]
 [assembly: System.Reflection.AssemblyProductAttribute("task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
